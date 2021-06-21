964f65b1be473fae7070d82d854d663ea5394c8e
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-06-21 17:01:31 -0600

    push
    updated assignment

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -11,20 +11,24 @@ namespace ConsoleAppAssignment
         static void Main(string[] args)
         {
             int i = 0;
+            // this is a while loop
             while (i < 10)
             {
+                // this will print out i = 0 to 1 = 9
                 Console.WriteLine("i = {0}", i);
                 i++;
             }
 
 
             int j = 0;
+            // this is a do while loop 
             do
             {
                 Console.WriteLine("j = {0}", j);
                 j++;
                 
                 if (j > 5)
+                    // this will print out j = 0 to j = 5 because the break will make it stop at 5 
                     break;
 
             } while (j < 10);
