b9a43f87ebbce1e2e67c358420baccf2ce38c3de
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-07 15:55:45 -0600

    push
    finished assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Param.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+   public class Param
+    {
+        public int Method1 (int x, int y = 0)
+        {
+            int result = x + y;
+            // returns the result which is x + y
+            return result;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a nunber: ");
+            int randomNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pick another number: ");
+            string randomStr = Console.ReadLine();
+
+            Param param = new Param();
+
+            if (randomStr == "")
+            {
+                Console.WriteLine(param.Method1(randomNum));
+            }
+
+            else
+            {
+                int number = Convert.ToInt32(randomStr);
+                // prints off Method1 which is x + y after the user picks two numbers which will come from randomNum and number 
+                Console.WriteLine(param.Method1(randomNum, number));
+            }
+            Console.ReadLine();
+        }
+    }
+}
