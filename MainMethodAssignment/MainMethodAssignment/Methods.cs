5550fe1d9016394c212dd423ce51186344d229f9
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-09 16:51:08 -0600

    push
    finished assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Methods
+    {
+        public int myMethod1(int num)
+        {
+            // prints out the sum on the console
+            return num / 4;
+        }
+
+
+        public int myMethod2(decimal d)
+        {
+            return Convert.ToInt32(d % 10);
+        }
+
+        public int myMethod3(string s)
+        {
+            // converts string into int 
+            int result = Convert.ToInt32(s) + 1000;
+            // returns int result 
+            return result;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+
+            // prints out the sum of 24 / 4
+            Console.WriteLine(methods.myMethod1(24));
+
+            // prints out the sum of 5.5 % 10
+            Console.WriteLine(methods.myMethod2(5.5m));
+
+            // prints out 33 + 1000
+            Console.WriteLine(methods.myMethod3("33"));
+
+            Console.ReadLine();
+        }
+    }
+}
