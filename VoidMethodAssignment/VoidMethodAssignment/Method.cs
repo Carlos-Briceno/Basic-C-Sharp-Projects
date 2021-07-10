5283f0452ab9ef0a04dccabf27738945e2caaad5
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-09 19:43:57 -0600

    push
    finished assignment

---
+++ VoidMethodAssignment/VoidMethodAssignment/Method.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Method
+    {
+        public void myMethod (int x, int y)
+        {
+            int operation = x + 12;
+            Console.WriteLine(operation);
+
+            Console.WriteLine(y);
+
+            // grabs operation and displays the sum
+            Console.WriteLine("The sum of x and 12 is " + operation);
+            // grabs y and displays the value of y
+            Console.WriteLine("The value of y is " + y);
+        }
+    }
+}
