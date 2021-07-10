9b100509e3788183a0c9b33bbba2be5d6b22b692
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-10 15:09:17 -0600

    push
    Finished assignment

---
+++ ClassAssignment/ClassAssignment/Method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Method
+    {
+        public static void outPut(int x, int y)
+        {
+            int num1 = x / 2;
+            int num2 = y;
+
+            // prints out whats in the parenthesis plus num1 which is whatever number the user inputs divided by 2
+            Console.WriteLine("First number divided by 2 = " + num1);
+
+            // prints out second number is num2 which will be what number a user puts into the console
+            Console.WriteLine("Second number is " + num2);
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose another number: ");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            // grabs ouPut method and displays userInput and user Input2
+            Method.outPut(userInput, userInput2);
+            Console.ReadLine();
+
+        }
+    }
+}
