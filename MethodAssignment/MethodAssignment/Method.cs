5e278442cb1d7c4b2ed16a4b9e5d852d57c90027
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-09 19:05:27 -0600

    push
    finished assignment

---
+++ MethodAssignment/MethodAssignment/Method.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodAssignment
+{
+   public class Method
+    {
+        static public void myMethod(int x, int y = 4)
+        {
+            Console.WriteLine(x - y);
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prints please enter a number to the console
+            Console.WriteLine("Please enter a number: ");
+            // converts num1 to an int 
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            // grabs mymethod from method.cs file and uses x - y than prints the sum onto the console
+            Method.myMethod(num1);
+
+            Console.WriteLine("Enter a second if you would like. ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Method.myMethod(num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
