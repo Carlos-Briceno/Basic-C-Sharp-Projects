d1a4812283b4eed7c10e24b33dc9ae8ba4dcdb59
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-11 01:23:55 -0600

    push
    Finished assignment

---
+++ MethodAndObjectsAssignment/MethodAndObjectsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            // grabs FirstName from Person file and than prints out Sample to console 
+            employee.FirstName = "Sample";
+            // grabs LastName from Person file than prints out student to console 
+            employee.LastName = "Student";
+            employee.PersonName();
+        }
+    }
+}
