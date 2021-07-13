6d38af4ee907f4512a7f7b54d576f15cc9282b03
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-12 22:47:55 -0600

    push
    updated assignment

--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -7,19 +7,30 @@ namespace MethodAssignment
     {
         static void Main(string[] args)
         {
-            Method method = new Method();
+            try
+            {
+                
+                Method method = new Method();
             
-            // prints please enter a number to the console
-            Console.WriteLine("Please enter a number: ");
-            // converts num1 to an int 
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            // grabs mymethod from method.cs file and uses x - y than prints the sum onto the console
-            method.myMethod(num1);
+                // prints please enter a number to the console
+                Console.WriteLine("Please enter a number: ");
+                // converts num1 to an int 
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                // grabs mymethod from method.cs file and uses x - y than prints the sum onto the console
+                Console.WriteLine(method.myMethod(num1));
 
-            Console.WriteLine("Enter a second if you would like. ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            method.myMethod(num1, num2);
-            Console.ReadLine();
+                Console.WriteLine("Enter a second if you would like. ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(method.myMethod(num1, num2));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
