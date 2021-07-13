5662fc1a9bc255079d7ed9d6414c660e82f4e985
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-13 16:54:24 -0600

    push

--- ClassAssignment/ClassAssignment/Method.cs
+++ ClassAssignment/ClassAssignment/Method.cs
@@ -19,5 +19,12 @@ namespace ClassAssignment
             // prints out second number is num2 which will be what number a user puts into the console
             Console.WriteLine("Second number is " + num2);
         }
+        
+        static void outPut2(int x, out int y)
+        {
+            y = 4;
+            int results = x * y;
+            y = results / 2;
+        }
     }
 }
