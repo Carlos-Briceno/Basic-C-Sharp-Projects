1c0032c253e4edd262c06c399def8f64c9413f7d
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-14 15:48:30 -0600

    push
    Finished Assignment

---
+++ PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            // prints off to the console 
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+        public void Quit()
+        {
+            Console.Write("Has quit");
+        }
+    }
+}
---
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee { FirstName = "Sample", LastName = "Student" };
+
+            IQuittable quittable = new Employee();
+
+            // prints Name: sample student
+            employee.SayName();
+            // prints out has quit into the console 
+            quittable.Quit();
+            Console.ReadLine();
+        }
+    }
+}
