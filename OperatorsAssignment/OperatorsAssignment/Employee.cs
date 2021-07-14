b7277c540b9d4169d766556da44ef0571a4d7527
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-14 17:51:31 -0600

    push
    Finished Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee : Person
+    {
+        public int Id = 0;
+
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return a.Id != b.Id;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates a new employee with a name 
+            Employee worker1 = new Employee("Harry", "Potter");
+            Employee worker2 = new Employee("Bob", "Ross");
+
+            // prints this to the console
+            Console.WriteLine("Are the employees the same?");
+            // prints result to the console either true or false
+            Console.WriteLine("Result: " + Convert.ToString(worker1 == worker2));
+            Console.ReadLine();
+        }
+    }
+}
