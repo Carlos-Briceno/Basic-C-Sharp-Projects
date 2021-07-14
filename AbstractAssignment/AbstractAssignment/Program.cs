d8c2add41183ed9ecd3b21f7b2fae14669fc43f6
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-07-13 19:43:15 -0600

    push
    finished assignment

---
+++ AbstractAssignment/AbstractAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee { FirstName = "Sample", LastName = "Student" };
+
+            // grabs employee ConsoleWriteLine and calls SayName method to print to the console 
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
