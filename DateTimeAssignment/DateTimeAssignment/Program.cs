267159976e13d2c62dbcc1b994fa92ac4cce0c09
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-08-10 19:20:19 -0600

    push

--- DateTimeAssignment/DateTimeAssignment/Program.cs
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -10,16 +10,19 @@ namespace DateTimeAssignment
     {
         static void Main(string[] args)
         {
+            // this is grabbing the current time from today 
             DateTime currentTime = DateTime.Now;
             Console.WriteLine(currentTime);
             Console.WriteLine("Please enter a number: ");
             try
             {
+                // converts num1 to an int
                 int num1 = Convert.ToInt32(Console.ReadLine());
                 TimeSpan futureTime = new TimeSpan(num1, 0, 0);
                 DateTime addedTime = currentTime.Add(futureTime);
                 if (num1 <= 1)
                 {
+                    // grabs user input and than adds it to the current time 
                     Console.WriteLine("in " + num1 + " hour, the time will be " + addedTime);
                 }
                 else
@@ -29,6 +32,7 @@ namespace DateTimeAssignment
             }
             catch (FormatException)
             {
+                // if the number is not entered than this error message will print to the console
                 Console.WriteLine("Please type a number");
                 return;
             }
