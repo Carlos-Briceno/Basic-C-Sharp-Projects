e4e238bfdeb3de03e25771a564d5d5194414c27d
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-08-15 13:14:09 -0600

    push
    updated assignment

---
+++ ConsteuctorAssignment/ConsteuctorAssignment/Const.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsteuctorAssignment
+{
+    class Const
+    {
+        public string Name;
+        public string School;
+
+        public Const(string name = "", string school = "")
+        {
+            this.Name = name;
+            this.School = school;
+        }
+    }
+}
---
+++ ConsteuctorAssignment/ConsteuctorAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsteuctorAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string Name = "Carlos Briceno";
+            var School = "The Tech Academy";
+
+            Console.WriteLine("{0} is attending {1}.", Name, School);
+            // creates a newPerson which will be used in the next line of code 
+            var newPerson = new Const("Mike Scott","The Tech Academy");
+            // chains two constructors togther which are Name and School and also prints to the console
+            Console.WriteLine($"{newPerson.Name} is attending {newPerson.School}");
+            Console.ReadLine();
+        }
+    }
+}
