0c64637a5fb2730539a0d741e91db9d54b948299
Carlos-Briceno <73920679+Carlos-Briceno@users.noreply.github.com>
2021-08-16 16:33:01 -0600

    push
    updated assignment

---
+++ ConsteuctorAssignment/ConsteuctorAssignment/Const.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsteuctorAssignment
+{
+   public class Const
+    {
+            public Const(float age1) : this(age1, "Carlos")
+            {
+                Console.WriteLine(age1 + " and your name is " + Name);
+            }
+            public Const(string name1) : this(23, name1)
+            {
+                Console.WriteLine(name1 + " and your age is " + Age);
+            }
+            public Const(float age, string name)
+            {
+                Age = age;
+                Name = name;
+            }
+            float Age { get; set; }
+            string Name { get; set; }
+        }
+    }
---
+++ ConsteuctorAssignment/ConsteuctorAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsteuctorAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string newName = "My name is ";
+            const string Age = "My age is ";
+            // asks user to enter their age or name 
+            Console.WriteLine("Enter your age or name: ");
+            var answer = Console.ReadLine();
+            if (float.TryParse(answer, out float x))
+            {
+                // prints "My age is " to the console 
+                Console.WriteLine(Age);
+                Const newChain = new Const(x);
+            }
+            else
+            {
+                // priints "My name is " to the console 
+                Console.WriteLine(newName);
+                Const newChain = new Const(answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
